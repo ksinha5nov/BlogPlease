306d96603572c43b31bb74198a5fe204587bc7c9
ksinha5nov <ksinha5nov@gmail.com>
2024-09-06 22:24:00 -0700

    added edit post feature, fixed styling

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import {Navigate} from "react-router-dom";
 import 'react-quill/dist/quill.snow.css';
+import Editor from "../Editor";
 
 export default function CreatePost(){
     const [title, setTitle] = useState("");
@@ -29,14 +30,14 @@ export default function CreatePost(){
 
 
     if(redirect){
-        <Navigate to={'/'} />
+        return <Navigate to={'/'} />
     }
     return (
         <form onSubmit={createNewPost}>
             <input type="title" placeholder={'Title'} value={title} onChange={ev => setTitle(ev.target.value)}/>
             <input type="summary" placeholder={'summary'} value={summary} onChange={ev => setSummary(ev.target.value)}/>
             <input type="file" onChange={ev => setFiles(ev.target.files)}/>
-            <ReactQuill value={content} onChange={newValue => setContent(newValue)}/>
+            <Editor value={content} onChange={setContent} />
             <button style={{marginTop: '5px'}}>Create Post</button>
         </form>
     )
